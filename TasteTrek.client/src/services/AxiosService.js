44e918a114a67eb6a571020164f1c04735f7b68e
Savannah Temple <savtemple@gmail.com>
2022-11-23 00:16:03 -0700

    need to figma first, axios added

--- TasteTrek.client/src/services/AxiosService.js
+++ TasteTrek.client/src/services/AxiosService.js
@@ -4,3 +4,10 @@ export const api = Axios.create({
   baseURL,
   timeout: 8000
 })
+
+
+// need to create a profile and request API key in order to access API
+export const wineApi = Axios.create({
+  baseURL: 'https://www.globalwinescore.com/',
+  timeout: 8000
+})
