9d2841f777cbe033abf38334304b3dabbe1423a3
Savannah Temple <savtemple@gmail.com>
2022-11-22 21:51:35 -0700

    fin basic contr/serv/mod - test in postman ahh

--- TasteTrek/server/controllers/WinesController.js
+++ TasteTrek/server/controllers/WinesController.js
@@ -3,9 +3,14 @@ import BaseController from "../utils/BaseController.js";
 
 export class WinesController extends BaseController {
   constructor() {
-    super(`/api/wines`);
-    this.router;
-    // .get('', this.getWines)
+    super("/api/wines")
+    this.router
+      .get('', this.getWines)
+      .get('/:wineId', this.getWineById)
+      // .use('')
+      .post('', this.createWine)
+      .put('/:wineId', this.editWine)
+      .delete('/:wineId', this.deleteWine)
   }
 
   async getWines(req, res, next) {
@@ -14,6 +19,44 @@ export class WinesController extends BaseController {
       res.send(wines);
     } catch (error) {
       next(error);
+    }
+  }
+
+  async getWineById(req, res, next) {
+    try {
+      let wine = await winesService.getWineById(req.params.wineId);
+      res.send(wine);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async createWine(req, res, next){
+    try {
+      let wineData = req.body
+      let wine = await winesService.createWine(wineData)
+      res.send(wine)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editWine(req, res, next){
+    try {
+      let wineData = req.body
+      let wine = await winesService.editWine(req.params.wineId, wineData)
+      res.send(wine)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async deleteWine(req, res, next){
+    try {
+      let wine = await winesService.deleteWine(req.params.wineId)
+      res.send(wine)
+    } catch (error) {
+      next(error)
     }
   }
 }
--- TasteTrek/server/services/WinesService.js
+++ TasteTrek/server/services/WinesService.js
@@ -1,9 +1,44 @@
+import { BadRequest } from "@bcwdev/auth0provider/lib/Errors.js";
 import { dbContext } from "../db/DbContext.js";
 
 class WinesService {
   async getWines() {
     const wines = await dbContext.Wines.find();
     return wines;
+  }
+
+  async getWineById(wineId) {
+    let wine = await dbContext.Wines.findById(wineId);
+    if (!wine) {
+      throw new BadRequest("Invalid Wine Id");
+    }
+    return wine;
+  }
+
+  async createWine(wineData){
+    let wine = await dbContext.Wines.create(wineData)
+    return wine
+  }
+
+  async editWine(wineId, wineData){
+    let wine = await this.getWineById(wineId)
+
+    wine.name = wineData.name || wine.name
+    wine.color = wineData.color || wine.color
+    wine.producer_name = wineData.producer_name || wine.producer_name
+    wine.producer_region = wineData.producer_region || wine.producer_region
+    wine.description = wineData.description || wine.description
+    wine.imgUrl = wineData.imgUrl || wine.imgUrl
+    wine.price = wineData.price || wine.price
+
+    await wine.save()
+    return wine
+  }
+
+  async deleteWine(wineId){
+    let wine = await this.getWineById(wineId)
+    await wine.remove()
+    return wine
   }
 }
 
